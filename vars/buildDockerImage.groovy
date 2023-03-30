f97b4599018a017b7791baac4fc6d2caa45708e2
0marSater <omar.buis22@gmail.com>
2023-03-30 17:40:53 +0200

    update buildDockerImage method

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,7 +1,7 @@
 
 
 def call() {
-    echo "building the docker image..."
+    echo "building the docker image: ${FULL_IMAGE_NAME}"
     sh "cd /var/jenkins_home/workspace/${JOB_NAME} && docker build -t ${FULL_IMAGE_NAME} ."
     // or replace ${PIPELINE_NAME} with implicit variable JOB_NAME.
 
