cfc889776bef5e390c82523caf643bd2e84873fc
0marSater <omar.buis22@gmail.com>
2023-03-30 17:14:18 +0200

    modify buldingDockerImage method

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,6 +1,9 @@
 
 
-def call(){
-    echo "building docker image .."
+def call() {
+    echo "building the docker image..."
+    sh "docker build -t ${FULL_IMAGE_NAME} ."
+
+    // flag to determine whether the build goes as expected or not.
+    IS_IMAGE_BUILT = true
 }
- 
---
+++ vars/login.groovy
@@ -0,0 +1,6 @@
+
+def call(){
+    echo "login to docker hub..."
+    echo "docker"
+}
+
