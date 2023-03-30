5195ec3e7e47eb3052a0fb254e995e8e83316c81
0marSater <omar.buis22@gmail.com>
2023-03-30 18:01:52 +0200

    Update buildDockerImage.groovy

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -2,8 +2,7 @@
 
 def call(FULL_IMAGE_NAME) {
     echo "building the docker image: ${FULL_IMAGE_NAME}"
-    echo "$WORKSPACE"
-    sh "cd ${WORKSPACE} && docker build -t ${FULL_IMAGE_NAME} ."
+    sh "cd ${WORKSPACE} && sudo docker build -t ${FULL_IMAGE_NAME} ."
 
 
     // flag to determine whether the build goes as expected or not.
