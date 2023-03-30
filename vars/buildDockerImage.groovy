2b0d2404d9f7ec5431562a22b212e621aa8c0fe7
0marSater <omar.buis22@gmail.com>
2023-03-30 17:54:58 +0200

    test changes

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -2,8 +2,8 @@
 
 def call(FULL_IMAGE_NAME) {
     echo "building the docker image: ${FULL_IMAGE_NAME}"
-    echo "$JOB_NAME"
-    sh "cd /var/jenkins_home/workspace_${JOB_NAME} && docker build -t ${FULL_IMAGE_NAME} ."
+    echo "$JOB_BASE_NAME"
+    sh "cd /var/jenkins_home/workspace/${JOB_BASE_NAME} && docker build -t ${FULL_IMAGE_NAME} ."
 
 
     // flag to determine whether the build goes as expected or not.
