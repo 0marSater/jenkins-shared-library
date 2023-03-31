944383aecb4a3240b8178aea4d15f97b86c8e173
0marSater <omar.buis22@gmail.com>
2023-03-31 14:31:15 +0200

    Update login.groovy

--- vars/login.groovy
+++ vars/login.groovy
@@ -1,7 +1,7 @@
 
 def call(){
     echo "login to docker hub..."
-    withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'dockerHub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh "echo $PASS | docker login -u $USER --password-stdin"
     }
 }
