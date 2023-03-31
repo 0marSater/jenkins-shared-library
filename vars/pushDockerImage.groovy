8da67e04421fa94c87c54cc1df93d7e3c056b2fb
0marSater <omar.buis22@gmail.com>
2023-03-31 03:13:00 +0200

    modify pushDockerImage mehtod

--- vars/pushDockerImage.groovy
+++ vars/pushDockerImage.groovy
@@ -1,4 +1,6 @@
 def call(FULL_IMAGE_NAME){
     echo "Pushing ${FULL_IMAGE_NAME}"
-    sh "docker push omarsater/private-repo:${FULL_IMAGE_NAME}"
+    def fullTag = sh(script: "docker tag $FULL_IMAGE_NAME omarsater/private-repo:$FULL_IMAGE_NAME", returnStdout: true).trim()
+    echo "$fullTag"
+//    sh "docker push omarsater/private-repo:${FULL_IMAGE_NAME}"
 }
